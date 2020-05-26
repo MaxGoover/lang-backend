fe6d1b57e2313a0fc49fd31f0cbb1d15f9df976d
maxgoover <maxgoover@gmail.com>
2020-05-26 12:22:13 +0300

    Added env params

--- config/web.php
+++ config/web.php
@@ -7,27 +7,27 @@ $mongodb = require __DIR__ . '/mongodb.php';
 $params = require __DIR__ . '/params.php';
 
 $config = [
-    'aliases' => [
+    'aliases'    => [
         '@bower' => '@vendor/bower-asset',
         '@npm'   => '@vendor/npm-asset',
     ],
-    'as access' => [
-        'class' => mdm\admin\components\AccessControl::class,
+    'as access'  => [
+        'class'        => mdm\admin\components\AccessControl::class,
         'allowActions' => [
             '*' // пока что доступ ко всему '*', потом уберем
         ]
     ],
-    'basePath' => dirname(__DIR__),
-    'bootstrap' => ['log'],
+    'basePath'   => dirname(__DIR__),
+    'bootstrap'  => ['log'],
     'components' => [
-        'authManager' => [
-            'class' => yii\rbac\DbManager::class,
+        'authManager'  => [
+            'class'        => yii\rbac\DbManager::class,
             'defaultRoles' => ['guest'],
         ],
-        'cache' => [
+        'cache'        => [
             'class' => yii\caching\FileCache::class,
         ],
-        'db' => $db,
+        'db'           => $db,
         'errorHandler' => [
             'errorAction' => 'site/error',
         ],
@@ -38,57 +38,59 @@ $config = [
                 ],
             ],
         ],
-        'log' => [
+        'log'          => [
             'traceLevel' => YII_DEBUG ? 3 : 0,
-            'targets' => [
+            'targets'    => [
                 [
-                    'class' => yii\log\FileTarget::class,
+                    'class'  => yii\log\FileTarget::class,
                     'levels' => ['error', 'warning'],
                 ],
             ],
         ],
-        'mailer' => [
-            'class' => yii\swiftmailer\Mailer::class,
+        'mailer'       => [
+            'class'            => yii\swiftmailer\Mailer::class,
             // send all mails to a file by default. You have to set
             // 'useFileTransport' to false and configure a transport
             // for the mailer to send real emails.
             'useFileTransport' => true,
         ],
-        'mongodb' => $mongodb,
-        'request' => [
+        'mongodb'      => $mongodb,
+        'request'      => [
             // !!! insert a secret key in the following (if it is empty) - this is required by cookie validation
             'cookieValidationKey' => env('COOKIE_VALIDATION_KEY'),
         ],
-        'urlManager' => [
+        'urlManager'   => [
             'enablePrettyUrl' => true,
-            'rules' => [],
-            'showScriptName' => false,
+            'rules'           => [],
+            'showScriptName'  => false,
         ],
-        'user' => [
+        'user'         => [
             'identityClass' => User::class,
 //            'enableAutoLogin' => true,
             'enableSession' => true,
-            'loginUrl' => ['rbac/user/login'],
+            'loginUrl'      => ['rbac/user/login'],
         ],
     ],
-    'id' => 'basic',
-    'language' => env('APP_LANGUAGE'),
-    'modules' => [
+    'id'         => env('APP_ID'),
+    'language'   => env('APP_LANGUAGE'),
+    'modules'    => [
         'rbac' => [
-            'class' => mdm\admin\Module::class,
+            'class'         => mdm\admin\Module::class,
             'controllerMap' => [
                 'assignment' => [
-                    'class' => mdm\admin\controllers\AssignmentController::class,
+                    'class'         => mdm\admin\controllers\AssignmentController::class,
                     'userClassName' => User::class,
-                    'idField' => '_id',
+                    'idField'       => '_id',
                     'usernameField' => 'username',
                 ],
             ],
-            'layout' => 'left-menu',
-            'mainLayout' => '@app/views/layouts/main.php',
+            'layout'        => 'left-menu',
+            'mainLayout'    => '@app/views/layouts/main.php',
         ]
     ],
-    'params' => $params,
+    'name'       => env('APP_NAME'),
+    'params'     => $params,
+    'timeZone'   => env('APP_TIME_ZONE'),
 ];
 
 if (YII_ENV_DEV) {

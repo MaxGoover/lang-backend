b872148a18ce6b740919232da2b1fb1c066f33fb
maxgoover <maxgoover@gmail.com>
2020-06-01 12:15:32 +0300

    Added settings for CORS

--- config/params.php
+++ config/params.php
@@ -4,4 +4,13 @@ return [
     'adminEmail' => 'admin@example.com',
     'senderEmail' => 'noreply@example.com',
     'senderName' => 'Example.com mailer',
+
+    // Настройки Cors для пользования API
+    'apiCorsOptions'              => [
+        'Origin'                           => [env('CORS_ORIGIN')],
+        'Access-Control-Request-Method'    => ['POST'], // методы, которые мы разрешаем
+        'Access-Control-Request-Headers'   => ['*'], // разрешаем пока что все заголовки
+        'Access-Control-Allow-Credentials' => null,
+        'Access-Control-Max-Age'           => 86400,
+    ],
 ];

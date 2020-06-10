7667830c1f112ef8dd975a36376bc8e087beb0c5
maxgoover <maxgoover@gmail.com>
2020-06-10 18:10:53 +0300

    Deleted UserService

--- repositories/UserRepository.php
+++ repositories/UserRepository.php
@@ -3,20 +3,11 @@
 namespace app\repositories;
 
 use app\models\user\User;
-use app\services\user\UserService;
-use yii\base\Model;
 use yii\web\NotFoundHttpException;
 use yii\web\ServerErrorHttpException;
 
 class UserRepository
 {
-    private UserService $_userService;
-
-    public function __construct(UserService $userService)
-    {
-        $this->_userService = $userService;
-    }
-
     public static function save (User $user) {
         if (!$user->save(false)) {
             throw new ServerErrorHttpException('Model save error');

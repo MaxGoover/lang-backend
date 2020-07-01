65b142c3418a8df99b16e78511d7143e28dd0e5d
maxgoover <maxgoover@gmail.com>
2020-07-01 10:16:06 +0300

    Bad commit

--- repositories/grammar/LevelReadRepository.php
+++ repositories/grammar/LevelReadRepository.php
@@ -5,13 +5,38 @@ namespace app\repositories\grammar;
 use app\models\level\Level;
 use yii\data\ActiveDataProvider;
 use yii\data\DataProviderInterface;
+use yii\db\Query as MysqlQuery;
 
 class LevelReadRepository
 {
-    public function getAll(): DataProviderInterface
+    public function getAll()
     {
-        $query = Level::find()->with('trainings')->all();
-        return $this->_getProvider($query);
+        $select = [
+            'level' => "JSON_OBJECT(
+                'id', lv.id,
+                'title', lv.title
+            )",
+            'trainings' => "JSON_OBJECT(
+                'title', tr.title,
+                'translation', tr.translation,
+                'alias', tr.alias,
+                'avatar', tr.avatar
+            )"
+        ];
+
+        $query = new MysqlQuery;
+        $query
+            ->from('level as lv')
+            ->leftJoin('training AS tr', 'tr.level_id = lv.id');
+
+        // Select
+        $query->select($select);
+
+        // Group by task ID
+//        $query->groupBy('level_id');
+
+        return $query->all();
+//        return $this->_getProvider($query);
     }
 
     private function _getProvider($query): ActiveDataProvider

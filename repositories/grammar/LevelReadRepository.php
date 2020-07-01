e1475c34c80029a22d136b4b942a428706ac78dc
maxgoover <maxgoover@gmail.com>
2020-07-01 18:21:49 +0300

    Bad commit

--- repositories/grammar/LevelReadRepository.php
+++ repositories/grammar/LevelReadRepository.php
@@ -3,6 +3,7 @@
 namespace app\repositories\grammar;
 
 use app\models\level\Level;
+use app\models\training\Training;
 use yii\data\ActiveDataProvider;
 use yii\data\DataProviderInterface;
 use yii\db\Query as MysqlQuery;
@@ -11,32 +12,33 @@ class LevelReadRepository
 {
     public function getAll()
     {
-        $select = [
-            'level' => "JSON_OBJECT(
-                'id', lv.id,
-                'title', lv.title
-            )",
-            'trainings' => "JSON_OBJECT(
-                'title', tr.title,
-                'translation', tr.translation,
-                'alias', tr.alias,
-                'avatar', tr.avatar
-            )"
-        ];
+        $query = new MysqlQuery();
 
-        $query = new MysqlQuery;
-        $query
-            ->from('level as lv')
-            ->leftJoin('training AS tr', 'tr.level_id = lv.id');
 
-        // Select
-        $query->select($select);
 
-        // Group by task ID
-//        $query->groupBy('level_id');
-
-        return $query->all();
+//        Product::find()->andWhere(['brand_id' => $id])->exists();
+//
+//        $query = Training::find()->alias('t')->active('p');
+//        $query->joinWith(['level ta'], false);
+//        $query->andWhere(['ta.tag_id' => $tag->id]);
+//        $query->groupBy('p.id');
 //        return $this->_getProvider($query);
+//
+//        SELECT * FROM Products
+//        WHERE EXISTS
+//        (SELECT * FROM Orders WHERE Orders.ProductId = Products.Id)
+
+//        'select * from training where level_id in (select id from level)'
+
+        return \Yii::$app->db->createCommand(
+            'SELECT * 
+                FROM level 
+                WHERE EXISTS (
+                    SELECT * 
+                    FROM training 
+                    WHERE training.level_id = level.id
+                )'
+        )->queryAll();
     }
 
     private function _getProvider($query): ActiveDataProvider

424985e2e07d4651e70a4437ba19e6459faac520
maxgoover <maxgoover@gmail.com>
2020-07-26 15:41:19 +0300

    Added new fill mongodb migration

--- TRASH.php
+++ TRASH.php
@@ -95,3 +95,25 @@
 //
 //
 //
+
+//Подскажите, пожалуйста, пытаюсь проверить, вызвался ли метод, который стоит в качестве обработчика онклика баттона.
+//
+//То есть создал экземпляр этого компонента, нахожу у него кнопку баттон (она одна) и вызываю ее клик. Проверяю вызвалась ли функция, которая стоит как обработчик этому клику. Однако expect фейлится..
+//
+//    const goNextSpy = jest.spyOn(formStepPersonalData.methods, 'goNext');
+//
+//    const wrapper = mount(formStepPersonalData, {
+//      propsData: {
+//    email: 'test@test.ru',
+//        birthDate: '01.01.2020',
+//        phone: '+79286666666',
+//        timerCount: 60,
+//      },
+//      mocks: {
+//    $v,
+//      },
+//    });
+//
+//    const buttonNext = wrapper.find('button');
+//    buttonNext.trigger('click');
+//    expect(goNextSpy).toHaveBeenCalled();

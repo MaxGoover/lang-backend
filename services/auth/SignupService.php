6e0ff14598e736565a1107d8745ebc8617f461bb
maxgoover <maxgoover@gmail.com>
2020-08-03 15:30:41 +0300

    Moved RoleManager.php

--- services/auth/SignupService.php
+++ services/auth/SignupService.php
@@ -6,7 +6,7 @@ use app\forms\auth\SignupForm;
 use app\identity\Identity;
 use app\models\user\User;
 use app\repositories\user\UserRepository;
-use app\managers\RoleManager;
+use app\other\managers\RoleManager;
 use app\access\Rbac;
 use Yii;
 

5245dc17fc576eb2b928be62b8a2f1802806decf
maxgoover <maxgoover@gmail.com>
2020-08-05 15:19:57 +0300

    Added cart controller and service

---
+++ controllers/shop/CartController.php
@@ -0,0 +1,128 @@
+<?php
+
+namespace app\controllers\shop;
+
+use shop\forms\Shop\AddToCartForm;
+use shop\readModels\Shop\ProductReadRepository;
+use shop\useCases\Shop\CartService;
+use Yii;
+use yii\filters\VerbFilter;
+use yii\web\Controller;
+use yii\web\NotFoundHttpException;
+
+class CartController extends Controller
+{
+    private $_goods;
+    private $_service;
+
+    public function __construct(
+        $id,
+        $module,
+        CartService $service,
+        ProductReadRepository $products,
+        $config = [])
+    {
+        parent::__construct($id, $module, $config);
+        $this->_products = $products;
+        $this->_service = $service;
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     * @throws NotFoundHttpException
+     */
+    public function actionAdd($id)
+    {
+        if (!$product = $this->_products->find($id)) {
+            throw new NotFoundHttpException('The requested page does not exist.');
+        }
+
+        if (!$product->modifications) {
+            try {
+                $this->_service->add($product->id, null, 1);
+                Yii::$app->session->setFlash('success', 'Success!');
+                return $this->redirect(Yii::$app->request->referrer);
+            } catch (\DomainException $e) {
+                Yii::$app->errorHandler->logException($e);
+                Yii::$app->session->setFlash('error', $e->getMessage());
+            }
+        }
+
+        $this->layout = 'blank';
+
+        $form = new AddToCartForm($product);
+
+        if ($form->load(Yii::$app->request->post()) && $form->validate()) {
+            try {
+                $this->_service->add($product->id, $form->modification, $form->quantity);
+                return $this->redirect(['index']);
+            } catch (\DomainException $e) {
+                Yii::$app->errorHandler->logException($e);
+                Yii::$app->session->setFlash('error', $e->getMessage());
+            }
+        }
+
+        return $this->render('add', [
+            'product' => $product,
+            'model' => $form,
+        ]);
+    }
+
+    /**
+     * @return mixed
+     */
+    public function actionIndex()
+    {
+        $cart = $this->_service->getCart();
+
+        return $this->render('index', [
+            'cart' => $cart,
+        ]);
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function actionQuantity($id)
+    {
+        try {
+            $this->_service->set($id, (int)Yii::$app->request->post('quantity'));
+        } catch (\DomainException $e) {
+            Yii::$app->errorHandler->logException($e);
+            Yii::$app->session->setFlash('error', $e->getMessage());
+        }
+        return $this->redirect(['index']);
+    }
+
+    /**
+     * @param $id
+     * @return mixed
+     */
+    public function actionRemove($id)
+    {
+        try {
+            $this->_service->remove($id);
+        } catch (\DomainException $e) {
+            Yii::$app->errorHandler->logException($e);
+            Yii::$app->session->setFlash('error', $e->getMessage());
+        }
+        return $this->redirect(['index']);
+    }
+
+    ##################################################
+
+    public function behaviors(): array
+    {
+        return [
+            'verbs' => [
+                'class' => VerbFilter::class,
+                'actions' => [
+                    'quantity' => ['POST'],
+                    'remove' => ['POST'],
+                ],
+            ],
+        ];
+    }
+}
---
+++ services/shop/CartService.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace app\services\shop;
+
+use shop\cart\Cart;
+use shop\cart\CartItem;
+use shop\repositories\Shop\ProductRepository;
+
+class CartService
+{
+    private $_cart;
+    private $_products;
+
+    public function __construct(Cart $cart, ProductRepository $products)
+    {
+        $this->_cart = $cart;
+        $this->_products = $products;
+    }
+
+    public function add($productId, $modificationId, $quantity): void
+    {
+        $product = $this->_products->get($productId);
+        $modId = $modificationId ? $product->getModification($modificationId)->id : null;
+        $this->_cart->add(new CartItem($product, $modId, $quantity));
+    }
+
+    public function clear(): void
+    {
+        $this->_cart->clear();
+    }
+
+    public function getCart(): Cart
+    {
+        return $this->_cart;
+    }
+
+    public function remove($id): void
+    {
+        $this->_cart->remove($id);
+    }
+
+    public function set($id, $quantity): void
+    {
+        $this->_cart->set($id, $quantity);
+    }
+}
